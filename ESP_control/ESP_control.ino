a780153d24bac54a41c3e6158754b846a6f9cf37
Anand Vardhan <133073364+Nandostream11@users.noreply.github.com>
2024-03-22 14:08:07 +0530

    Update ESP_control.ino
    The first version that is working good

--- ESP_control/ESP_control.ino
+++ ESP_control/ESP_control.ino
@@ -1,97 +1,86 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+#include <esp_now.h>
+#include <WiFi.h>
+//78:21:84:DE:16:D8
+// REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0x78, 0x21, 0x84, 0xDE, 0x16, 0xD8};
+#define Left_VRX_PIN 34
+#define Left_VRY_PIN 35
 
-#define Left_VRX_PIN 25
-#define Left_VRY_PIN 26
 
-int x_val;
-int y_val;
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  float x_val;
+  float y_val;
+} struct_message;
 
+// Create a struct_message called myData
+struct_message myData;
 
-// #define F 35  
-// #define L 32
-// #define R 33
+esp_now_peer_info_t peerInfo;
 
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
 void setup() {
-  // put your setup code here, to run once:
+  // Init Serial Monitor
   Serial.begin(115200);
   pinMode(Left_VRX_PIN, INPUT);
   pinMode(Left_VRY_PIN, INPUT);
-  // pinMode(F, INPUT); 
-  // pinMode(L, INPUT);
-  // pinMode(R, INPUT);
+ 
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
 
-}
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  x_val = map(analogRead(Left_VRX_PIN),0,4095,-100,255);//value 0-255 (">> 4" convert maximum value from 4095 to 255 )
-  y_val = map(analogRead(Left_VRY_PIN),0,4095,-100,255);//value 0-255  
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Transmitted packet
+  esp_now_register_send_cb(OnDataSent);
   
-  Serial.printf("x: %d, y: %d\n", x_val, y_val);
-
-  delay(100);
-  // message();
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
-
-// void message(){
-//   if()
-//   else
-
-// }
-
-// #include <esp_now.h>
-// #include <WiFi.h>
-
-// // Replace with the MAC address of the receiving ESP32
-// uint8_t receiverMacAddress[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-
-// const int xAxisPin = 34; // Analog pin for X-axis
-// const int yAxisPin = 35; // Analog pin for Y-axis
-
-// void setup() {
-//   Serial.begin(115200);
-
-//   // Set device as a Wi-Fi Station
-//   WiFi.mode(WIFI_STA);
-
-//   // Initialize ESP-NOW
-//   if (esp_now_init() != ESP_OK) {
-//     Serial.println("Error initializing ESP-NOW");
-//     return;
-//   }
-
-//   // Register peer
-//   esp_now_peer_info_t peerInfo;
-//   memcpy(peerInfo.peer_addr, receiverMacAddress, 6);
-//   peerInfo.channel = 0;
-//   peerInfo.encrypt = false;
-
-//   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-//     Serial.println("Failed to add peer");
-//     return;
-//   }
-// }
-
-// void loop() {
-//   int xValue = analogRead(xAxisPin); // Read X-axis value
-//   int yValue = analogRead(yAxisPin); // Read Y-axis value
-
-//   // Map the analog values (0-4095) to a range (0-255) for better readability
-//   int xMapped = map(xValue, 0, 4095, 0, 255);
-//   int yMapped = map(yValue, 0, 4095, 0, 255);
-
-//   // Prepare data to send
-//   String dataToSend = String(xMapped) + "," + String(yMapped);
-//   uint8_t data[dataToSend.length() + 1];
-//   dataToSend.getBytes(data, dataToSend.length() + 1);
-
-//   // Send data
-//   esp_err_t result = esp_now_send(receiverMacAddress, data, dataToSend.length() + 1);
-
-//   if (result == ESP_OK) {
-//     Serial.println("Data sent successfully");
-//   } else {
-//     Serial.println("Error sending data");
-//   }
-
-//   delay(100); // Adjust the delay based on your requirements
-// }
+ 
+void loop() {
+  // Set values to send
+ // myData.x_val=map(analogRead(Left_VRX_PIN),0,4095,0,255);
+  //myData.y_val =map(analogRead(Left_VRY_PIN),0,4095,0,255);
+  myData.x_val=map(analogRead(Left_VRX_PIN),0,4095,0,255);
+  myData.y_val =map(analogRead(Left_VRY_PIN),0,4095,0,255);
+  
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  
+}
